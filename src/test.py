c91ba591704eaee4ba0efbe56ffc90bafadac230
andi klein <pabloemma@gmail.com>
2026-03-29 18:09:27 +0200

    1028 hPa

--- src/test.py
+++ src/test.py
@@ -14,7 +14,7 @@ bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
 # change this to match the location's pressure (hPa) at sea level
 #for basel 1024
-bme280.sea_level_pressure = 1026
+bme280.sea_level_pressure = 1028
 
 while True:
     print("\nTemperature: %0.1f C" % bme280.temperature)

b9090ef1dbf88e1fb234bbd3617ab297c0a2bedb
andi klein <pabloemma@gmail.com>
2026-03-30 16:45:56 +0200

    first client

---
+++ src/MyClient.py
@@ -0,0 +1,29 @@
+# the BME280 client 
+
+import socket
+
+class MyClient(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = None
+
+    def connect(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.host, self.port))
+
+    def send(self, message):
+        if self.sock:
+            self.sock.sendall(message.encode('utf-8'))
+
+    def close(self):
+        if self.sock:
+            self.sock.close()
+            self.sock = None
+
+
+if __name__ == "__main__":
+    client = MyClient('192.168.3.201', 9378)
+    client.connect()
+    client.send("Hello, Server!")
+    client.close()

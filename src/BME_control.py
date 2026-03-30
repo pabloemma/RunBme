f276ec49c23a1793797f49e2afe2397e186c6f43
andi klein <pabloemma@gmail.com>
2026-03-30 20:07:24 +0200

    first commit

---
+++ src/BME_control.py
@@ -0,0 +1,7 @@
+# this is the code which runs on the raspberry pi connected to the BME280
+#it mainly runs the read loop and communicates with the server
+
+
+import time
+import MyClient as MY
+import ReadBme as RB

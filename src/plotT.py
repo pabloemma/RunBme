e818073da2e9ca1550be439db327129fd2a4e802
andi klein <pabloemma@gmail.com>
2026-04-04 14:47:24 +0200

    label in plot

--- src/plotT.py
+++ src/plotT.py
@@ -32,10 +32,11 @@ temp3 = data['temperature']*1.8+32
 
 #temp.plot()
 #temp.plot(ax=ax,marker='+',color='green',linestyle='None')
-temp.plot(ax=ax,color='green',linestyle='--')
+temp.plot(ax=ax,color='green',linestyle='--',label ='temperature in C')
 temp1.plot(ax=ax,color='red',linestyle='--')
 temp2.plot(ax=ax,color='blue',linestyle='--')
-temp3.plot(ax=ax,color='black',linestyle='--')                      
+temp3.plot(ax=ax,color='black',linestyle='--', label ='temperature in F') 
+ax.legend(loc='upper left')                     
 
 
 #This could also be done using

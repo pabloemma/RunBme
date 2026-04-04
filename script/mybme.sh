4fa605c2c9a0162d3fbec671c98d381b62bfa2dd
andi klein <pabloemma@gmail.com>
2026-04-04 15:33:14 +0200

    control script

---
+++ script/mybme.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+ 
+
+
+if [ $(ps -efa | grep -v grep | grep BME_control.py -c) -gt 0 ] ;
+then
+    echo "Process running ...";
+else
+#temporary if program dies download all the necessary programs
+#cd /home/pi/git/speedtest/src
+#./update_speedtest
+#cd ~
+/home/pi/activate_venv.sh
+python3 -u /home/pi/git/RunBme/src/BME_control.py.py  > /home/pi/BME_control.log 2>> /home/pi/BME_control_restart_error.log &
+
+
+echo "Starting the process";
+fi;
